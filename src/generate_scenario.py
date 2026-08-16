cd3389836e0c6fd3aa3ece831178e70a73a1dd02
Lucas <lucasbertin2007@gmail.com>
2026-08-16 10:50:11 +0200

    starting to put everuthing together + generate_scenario finished (i think)

--- src/generate_scenario.py
+++ src/generate_scenario.py
@@ -13,6 +13,13 @@ def generate_scenario(pipeline, config, zones, n_scenarios, *, dt, cell_area, fi
 
     for z in z_draws :
         theta = decode_free_parameters(jnp.asarray(z), config) # converts the vector z into a physical parameter
-        out = pipeline.direct(theta, frame_count=frame_count) # to make work the simulation
+        out = pipeline.direct(theta, frame_count = frame_count) # to make work the simulation
         fires.append(out["burned_area"])
         concentrations.append(out["concentration_frames"])
+    
+    scenarios_fire = jnp.stack(fires) # stack so we can have a 1D JAX array
+    concentration_scenarios = jnp.stack(concentrations)
+
+    scenarios_H_r = health_impact(concentration_scenarios, zones, dt = dt, cell_area = cell_area, filter_level = filter_level, )
+
+    return scenarios_fire, scenarios_H_r # we return the two matrix for optimize_portfolio() and generate_efficient_frontier
